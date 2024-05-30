9e300e7b67f95b39b36367b83d7132b46186bab3
MusaFK05 <87690344+MusaFK05@users.noreply.github.com>
2024-05-30 16:49:57 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,1 @@
 from .processor import SensorDataProcessor
-
-# class sensor:
-#     def __init__(self, df):
-#         self.df = df
-
-#     def labeling(self):
-#         process_data = SensorDataProcessor(self.df)
-#         process_data.apply_mappings()
-#         processed_df = process_data.process_data()
-#         return processed_df
-
-def labeling(df):
-    process_data = SensorDataProcessor(df)
-    process_data.apply_mappings()
-    processed_df = process_data.process_data()
-    return processed_df
